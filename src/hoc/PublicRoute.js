b7004f9bc3f5c00ccaa4f41a19a365920d4a513c
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-07-17 17:29:01 +0300

    Update PublicRoute.js

--- src/hoc/PublicRoute.js
+++ src/hoc/PublicRoute.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { selectLoggedIn } from 'redux/auth/authSelectors';
 
 export const PublicRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectLoggedIn);
-  const location = useLocation();
   if (isLoggedIn) {
-    return <Navigate to={location.state?.from ?? '/'} />;
+    return <Navigate to={'/contacts'} />;
   }
   return children;
 };
